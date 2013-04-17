266e057aedeaf27bd6c3fe51857d1c040b4cbe31
Mikhail S. Pobolovets <styx.mp@gmail.com>
2013-04-17 12:57:57 +0300

    Added data_bags support for PG users.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- recipes/pg_user.rb
+++ recipes/pg_user.rb
@@ -3,8 +3,21 @@
 # Recipe:: pg_user
 #
 
-# setup users
-node["postgresql"]["users"].each do |user|
+users = []
+
+if node['postgresql'].attribute?('users')
+  users = node["postgresql"]["users"]
+end
+
+begin
+  data_bag('postgresql_users').each do |item|
+    users << data_bag_item('postgresql_users', item)
+  end
+rescue
+  Chef::Log.info "Could not load data bag 'postgresql_users'"
+end
+
+users.each do |user|
   pg_user user["username"] do
     privileges :superuser => user["superuser"], :createdb => user["createdb"], :login => user["login"]
     password user["password"]
